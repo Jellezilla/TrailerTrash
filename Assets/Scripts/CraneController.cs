ea1f055f89616d4ba32971549893981d5fd86556
Jellezilla <jesper@tjorne.dk>
2015-11-17 13:54:38 +0100

    Initial commit

---
+++ Assets/Scripts/CraneController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraneController : MonoBehaviour {
+	
+	public GameObject Cube1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.J)) {
+		
+			SpawnNewObject();	
+		}
+	}
+
+
+	void SpawnNewObject() {
+
+
+
+		Vector3 pos = new Vector3 (0.0F, 5.0F, 0.0F);
+		(Instantiate(Cube1, pos, transform.rotation) as GameObject).transform.parent = transform;
+
+
+	}
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+
+	Rigidbody rigid;
+	public float speed = 0.30F;
+	// Use this for initialization
+	void Start () {
+		rigid = transform.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void FixedUpdate() {
+		if(Input.GetKeyDown (KeyCode.LeftArrow)) {
+			rigid.AddForce(-Vector3.right * speed, ForceMode.Force);
+
+		}
+		if(Input.GetKeyDown (KeyCode.RightArrow)) {
+			rigid.AddForce(Vector3.right * speed, ForceMode.Force);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			transform.position = new Vector3(0.0F,0.9F,0.0F);
+		}
+	}
+}
