d2d7e9221f5d81672fa83056764fbd504ecd7604
Jellezilla <jesper@tjorne.dk>
2015-11-18 07:25:37 +0100

    crane update

--- Assets/Scripts/CraneController.cs
+++ Assets/Scripts/CraneController.cs
@@ -4,9 +4,22 @@ using System.Collections;
 public class CraneController : MonoBehaviour {
 	
 	public GameObject Cube1;
+
+	private Rigidbody rigid;
+
+	float maxUpAndDown = 0.01F;             // amount of meters going up and down
+	float speed = 20.0F;
+	float maxSpeed = 20.0F;
+	private float angle = 0.0F;
+	private bool active;
+
+	private float toDegrees = Mathf.PI / 180;
 	// Use this for initialization
 	void Start () {
-	
+		rigid = transform.GetComponent<Rigidbody> ();
+		rigid.useGravity = false;
+		active = true;
+		StartCoroutine (SpawnCall (5.0F));
 	}
 	
 	// Update is called once per frame
@@ -15,16 +28,52 @@ public class CraneController : MonoBehaviour {
 		
 			SpawnNewObject();	
 		}
+		if (Input.GetKey (KeyCode.A)) {
+			rigid.AddForce(-Vector3.right * (speed/3), ForceMode.Force);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rigid.AddForce(Vector3.right * (speed/3), ForceMode.Force);
+		}
+
+	}
+	void FixedUpdate() {
+		if (active)
+		{
+			angle += speed * Time.deltaTime;
+			if (angle > 360) angle -= 360;
+			transform.position = new Vector3(transform.position.x + (maxUpAndDown * Mathf.Cos(angle * toDegrees)), transform.position.y, transform.position.z);
+		}
+
+
+		// restrictions
+		if(rigid.velocity.magnitude > maxSpeed)
+		{
+			rigid.velocity = rigid.velocity.normalized * maxSpeed;
+		}
+		if (transform.position.x < -1.43f) 
+			transform.position = new Vector3 (-1.42f, transform.position.y, transform.position.z);
+		if (transform.position.x > 1.43f) 
+			transform.position = new Vector3 (1.42f, transform.position.y, transform.position.z);
+		
 	}
 
-
 	void SpawnNewObject() {
+		
+		
+		
+		Vector3 pos = new Vector3 (transform.position.x, 5.0F, 0.0F);
+		//(Instantiate(Cube1, pos, transform.rotation) as GameObject).transform.parent = transform;
+		Instantiate (Cube1, pos, transform.rotation);
 
 
+	}
+	IEnumerator SpawnCall(float wait) {
+		SpawnNewObject ();
+		yield return new WaitForSeconds (wait);
 
-		Vector3 pos = new Vector3 (0.0F, 5.0F, 0.0F);
-		(Instantiate(Cube1, pos, transform.rotation) as GameObject).transform.parent = transform;
 
-
+		if (active) {
+			StartCoroutine(SpawnCall(Random.Range (3,5)));
+		}
 	}
 }
---
+++ Assets/Truck.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Truck : MonoBehaviour {
+
+	private float spd = 250.0F;
+	Rigidbody rigid;
+	bool active;
+	// Use this for initialization
+	void Start () {
+		active = false;
+		rigid = transform.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.U)) {
+			active = true;
+			Debug.Log ("fede!");
+		}
+		if (active) {
+			BackUp ();
+		}
+		if (transform.position.x < 4.5f) {
+			active = false;
+			StartCoroutine(DriveOff());
+		}
+	}
+
+	void FixedUpdate() {
+		if (rigid.velocity.magnitude > spd) {
+			rigid.velocity = rigid.velocity.normalized * spd;
+		}
+	}
+	private void BackUp() {
+		rigid.AddForce (-Vector3.right * spd, ForceMode.Force);
+	}
+
+	IEnumerator DriveOff() {
+
+		Transform trailer = GameObject.FindWithTag ("Trailer").transform;
+		trailer.parent = transform;
+
+		while (transform.position.x < 20.0f) {
+			rigid.AddForce(Vector3.right * (spd * 1.5f) , ForceMode.Force);
+			yield return null;	
+		}
+	}
+}
