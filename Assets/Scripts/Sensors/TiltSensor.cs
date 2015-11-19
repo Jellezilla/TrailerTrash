105eb93c2ec56870ef8fe17bdd6fe6ea652549ef
GitHocico <thomasravnholt@gmail.com>
2015-11-19 03:11:16 +0100

    Sensors Tilt/Weight
    - Tile with height difference can be called for when needed
    - How to do weight, still not nailed

---
+++ Assets/Scripts/Sensors/TiltSensor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TiltSensor : MonoBehaviour 
+{
+	public List<GameObject> edges = new List<GameObject>();
+
+	public float GetTilt()
+	{
+		return Mathf.Abs (edges[0].transform.position.y-edges[1].transform.position.y);
+	}
+}
---
+++ Assets/Scripts/Sensors/WeightSensor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightSensor : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log ("something hit. I wonder what it weighs?");
+	}
+}
