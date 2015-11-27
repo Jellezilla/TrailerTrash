4c147d54fc49bbab94360f3efb1a04bb8a123131
Jellezilla <jesper@tjorne.dk>
2015-11-27 11:39:56 +0100

    ttf

--- Assets/GUInterface.cs
+++ Assets/GUInterface.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+
 
 public class GUInterface : MonoBehaviour {
 
+	Font myFont;
+
+
 	// Use this for initialization
 	void Start () {
+		myFont = Resources.Load ("GROBOLD.ttf") as Font;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	void GUI() {
+		if (Application.loadedLevel == 0) {
+				
+		}
+		
+		GUIStyle myStyle = new GUIStyle();
+		myStyle.font = myFont;
+
+		//GUI.Label (new Rect (10, 10, 100, 30), "Hello World!", myStyle);
+
 	}
 }
