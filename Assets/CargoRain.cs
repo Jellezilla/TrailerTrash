79857ff2ec4808cd798174d6118e4b1387823229
Jellezilla <jesper@tjorne.dk>
2015-12-04 15:49:17 +0100

    MainMenu Scene

---
+++ Assets/CargoRain.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CargoRain : MonoBehaviour {
+
+
+	public Transform woodPrefab;
+	public Transform ironPrefab;
+	public Transform glassPrefab;
+
+	int rand;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rand = Random.Range (1, 20);
+		if (rand == 1) {
+			RainDaCargo();
+		}
+	
+	}
+	void RainDaCargo() {
+		int typeRand = Random.Range (1, 4);
+		Vector3 pos = GetRandSpawnPos ();
+		if (typeRand == 1) {
+			Instantiate(woodPrefab, pos, Quaternion.identity);
+		} else if (typeRand == 2) {
+			Instantiate(ironPrefab, pos, Quaternion.identity);
+		} else if (typeRand == 3) {
+			Instantiate(glassPrefab, pos, Quaternion.identity);
+		}
+
+	}
+
+	Vector3 GetRandSpawnPos () {
+		float randx = Random.Range (0.0f, 14.5f);
+		float randy = 5.0f;
+		float randz = Random.Range (-9.0f, -0.5f);
+		return new Vector3(randx, randy, randz);
+	}
+}
+
---
+++ Assets/menuTruck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuTruck : MonoBehaviour {
+	float speed;
+	float startX;
+	Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (10.05f, 18.15f);
+		startX = transform.position.x;
+		rigid = transform.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigid.AddForce ((Vector3.right *speed * Time.deltaTime), ForceMode.Force);
+		//transform.position = new Vector3 ((transform.position.x + rand), transform.position.y, transform.position.z);
+
+		if (transform.position.x > 15.0f) {
+			transform.position = new Vector3(startX, transform.position.y, transform.position.z);
+		}
+
+	
+	}
+
+	void FixedUpdate() {
+		if (rigid.velocity.magnitude > speed) {
+			rigid.velocity = rigid.velocity.normalized * speed;
+		}
+	}
+}
